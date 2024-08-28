e9dc03fd80feae7392e5b0f7a4308a67860903f0
hanjunpeng <1358271059@qq.com>
2024-08-28 10:26:53 +0800

    提交代码

--- src/components/ItasklistDetail.vue
+++ src/components/ItasklistDetail.vue
@@ -322,7 +322,7 @@ export default {
         setTimeout(() => {
           try{
             let height = this.$refs.taskRef.offsetHeight
-            top.updateSonTaskIframeHeight(height + 30)
+            top.updateSonTaskIframeHeight(height + 20)
           }catch(e) {
             console.log('获取总高度', e)
           }
