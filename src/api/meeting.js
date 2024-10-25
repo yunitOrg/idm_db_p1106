42397c1604d24cabce1e90be2a10f13b7147f3bf
liyang <liyangmsn@live.com>
2024-10-25 16:51:53 +0800

    fix: 意见反馈上传逻辑调整、新增表单暂存功能

--- src/api/meeting.js
+++ src/api/meeting.js
@@ -3,24 +3,8 @@ export default{
    * @Desc 意见反馈
    * @Author hjp
    */
-  async ApiIdearBack({file, content, params}) {
-    let formdata = new FormData();
-    for(let i=0; i<file.length; i++) {
-      let fid = file[i]
-      formdata.append('file', fid)
-    }
-    // noticeId && formdata.append('noticeId', noticeId)
-    content && formdata.append('content', content)
-    if (params) {
-      Object.keys(params).forEach(item => {
-        formdata.append(item, params[item])
-      })
-    }
-    const { data } = await window.IDM.http.post('ctrl/feedbackExt/feedback', formdata, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      },
-    })
+  async ApiIdearBack(params) {
+    const { data } = await window.IDM.http.post('ctrl/feedbackExt/feedback', params)
     return data
   }
 }
