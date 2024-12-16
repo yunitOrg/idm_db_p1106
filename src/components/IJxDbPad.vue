68625f694bab808ffd314e974b1ab4e91e3fa11b
liyang <liyangmsn@live.com>
2024-12-16 18:36:08 +0800

    feat: 添加江西督办PAD组件

---
+++ src/components/IJxDbPad.vue
@@ -0,0 +1,16 @@
+<template>
+    <div idm-ctrl="idm_module" :id="moduleObject.id" :idm-ctrl-id="moduleObject.id" @click="clickHandle" class="idm-db-IJxDbPad" :class="className.wrap"></div>
+</template>
+<script>
+import bindProp from '../mixins/bindProp'
+import bindStyle from '../mixins/bindStyle'
+export default {
+    mixins: [bindProp(), bindStyle()],
+    data() {
+        return {}
+    },
+    methods:{
+        
+    }
+}
+</script>
