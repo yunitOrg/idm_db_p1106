9ec1d0b945982cb9689c91d63f61c0a4fbce5c78
fxt1012198778 <fxt19990208@163.com>
2025-07-30 17:56:27 +0800

    fxt

--- src/components/IJxLeaderPad.vue
+++ src/components/IJxLeaderPad.vue
@@ -894,7 +894,7 @@ html {
 <style lang="scss" scoped>
 .idm-db-IJxLeaderPad-container {
     gap: 2.5rem;
-    background: #8fc7ff;
+    // background: #8fc7ff;
     overflow-y: hidden;
     .main-container {
         padding: 0 3.75rem 3.75rem;
